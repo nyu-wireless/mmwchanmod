8da462e1f1785c5b57b0610820a7bca3f0589b20
Sundeep Rangan <sdrangan@gmail.com>
2020-11-13 11:31:17 +0100

    Added search of sub-packages in set up.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name = 'mmwchanmod',
       version = '0.0',
-      description = 'Package for modeling and simulation of mmWave wireless channel',
-      author = 'GAMP Team',
+      description = 'Package for modeling and simulation of mmWave wireless channels',
+      author = 'Sundeep Rangan, William Xia, NYU',
       install_requires = ['numpy','scipy','matplotlib','tensorflow','scikit-learn',],
       author_email = 'srangan@nyu.edu',
       license = 'MIT',
-      packages = ['mmwchanmod'],
+      packages=find_packages(),      
       zip_safe = False)
